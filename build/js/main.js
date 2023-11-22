511292556bee9bdf4d45ad782eb246ac828ba52e
kimz code <81085401+KimzCodes@users.noreply.github.com>
2023-11-22 14:51:28 +0200

    init

---
+++ build/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+//create file html, main.ts and main.js
+//let us define myName and run tsc command
+//as u c we this command generate js file, but as u c js is old code var we will learn how to control the version of js after little bit
+//the error u c now because we open both file ts vs js,  close it and error will gone
+let myName = "hello";
+console.log(myName);
+//now let us try to organize our project little bit
+//create build folder move main.ts into it and src folder, then tsc --init
+//as u c tsconfig.ts has been created, we used this file to set some config for ts, such as js version
+//open ts config change "rootDir": "./src" and  "outDir": "./build/js" and
+//run tsc and check js file now, as u see there is no var now thats because
+//--> target setting u can find it in tsconfig, we use target to configure version of js output file
+//another command i want to share with u instead of tsc command try tsc --watch
+//now we it will re build js once u do update in ts file
+//another issue we have try to create ts file out of src folder, as u c it create new js file outside build
+//to fix it go to ts config add to end "include": ["src"]
